6709354feef8c4c78613e727419f8f057653c494
Giacomo Marciani <giacomo.marciani@gmail.com>
2015-09-24 17:02:11 +0200

    Test application creation

--- test/model/TestApplication.java
+++ test/model/TestApplication.java
@@ -10,37 +10,39 @@ import model.application.OperationalPath;
 import model.application.operator.ComputationalDemand;
 import model.application.operator.OperationalNode;
 import model.application.operator.Operator;
-import model.application.operator.OperatorType;
-import model.application.operator.Transformation;
-import model.application.operator.TransformationType;
+import model.application.operator.OperatorRole;
 
 public class TestApplication {
 
-	@Test public void normal() {
+	@Test 
+	public void normal() {
 		Application app = getSimpleApp();
 		
 		System.out.println(app);
 		
 	}
 	
-	@Test public void cycled() {
+	@Test 
+	public void cycled() {
 		Application app = getCycledApp();
 		
 		System.out.println(app);
 	}
 	
-	@Test public void pathsFromSourceToSink() {
+	@Test 
+	public void pathsFromSourceToSink() {
 		Application app = getSimpleApp();
 		
-		OperationalNode source = app.getOperationals().getSources().first();
-		OperationalNode sink = app.getOperationals().getSinks().first();
+		OperationalNode source = app.getVertices().stream().findAny().get();
+		OperationalNode sink = app.getVertices().stream().findAny().get();
 		
 		Set<OperationalPath> paths = app.getOperationalPaths(source, sink);
 		
 		System.out.println(paths);
 	}
 	
-	@Test public void allPaths() {
+	@Test 
+	public void allPaths() {
 		Application app = getSimpleApp();
 		
 		Set<OperationalPath> paths = app.getAllOperationalPaths();
@@ -51,20 +53,15 @@ public class TestApplication {
 	private static Application getSimpleApp() {
 		Application app = new Application("Simple DSP Application");
 		
-		OperationalNode node1 = new OperationalNode(1, new Operator(OperatorType.SRC, "gridsensor", new Transformation(TransformationType.EQUAL, 100)), new ComputationalDemand(1, 1.0), new LinkedHashSet<Long>());
-		OperationalNode node2 = new OperationalNode(2, new Operator(OperatorType.PIP, "selection1", new Transformation(TransformationType.RATIO, 0.5)), new ComputationalDemand(1, 1.0), new LinkedHashSet<Long>());
-		OperationalNode node3 = new OperationalNode(3, new Operator(OperatorType.PIP, "selection2", new Transformation(TransformationType.RATIO, 0.5)), new ComputationalDemand(1, 1.0), new LinkedHashSet<Long>());
-		OperationalNode node4 = new OperationalNode(4, new Operator(OperatorType.SNK, "datacenter", new Transformation(TransformationType.EQUAL, 0.0)), new ComputationalDemand(1, 1.0), new LinkedHashSet<Long>());
+		OperationalNode node1 = new OperationalNode(1, new Operator(OperatorRole.SRC, "gridsensor", x -> new Long(1000)), new ComputationalDemand(1, 1.0), new LinkedHashSet<Long>());
+		OperationalNode node2 = new OperationalNode(2, new Operator(OperatorRole.PIP, "selection1", x -> x/2), new ComputationalDemand(1, 1.0), new LinkedHashSet<Long>());
+		OperationalNode node3 = new OperationalNode(3, new Operator(OperatorRole.PIP, "selection2", x -> x/2), new ComputationalDemand(1, 1.0), new LinkedHashSet<Long>());
+		OperationalNode node4 = new OperationalNode(4, new Operator(OperatorRole.SNK, "datacenter", x -> new Long(1)), new ComputationalDemand(1, 1.0), new LinkedHashSet<Long>());
 		
-		app.addOperational(node1);
-		app.addOperational(node2);
-		app.addOperational(node3);
-		app.addOperational(node4);
-		
-		app.addStream(1, 2);
-		app.addStream(1, 3);
-		app.addStream(2, 4);
-		app.addStream(3, 4);
+		app.addStream(node1, node2);
+		app.addStream(node1, node3);
+		app.addStream(node2, node4);
+		app.addStream(node3, node4);
 		
 		return app;
 	}
@@ -72,21 +69,17 @@ public class TestApplication {
 	private static Application getCycledApp() {
 		Application app = new Application("Cycled DSP Application");
 		
-		OperationalNode node1 = new OperationalNode(1, new Operator(OperatorType.SRC, "gridsensor", new Transformation(TransformationType.EQUAL, 100)), new ComputationalDemand(1, 1.0), new LinkedHashSet<Long>());
-		OperationalNode node2 = new OperationalNode(2, new Operator(OperatorType.PIP, "selection1", new Transformation(TransformationType.RATIO, 0.5)), new ComputationalDemand(1, 1.0), new LinkedHashSet<Long>());
-		OperationalNode node3 = new OperationalNode(3, new Operator(OperatorType.PIP, "selection2", new Transformation(TransformationType.RATIO, 0.5)), new ComputationalDemand(1, 1.0), new LinkedHashSet<Long>());
-		OperationalNode node4 = new OperationalNode(4, new Operator(OperatorType.SNK, "datacenter", new Transformation(TransformationType.EQUAL, 0.0)), new ComputationalDemand(1, 1.0), new LinkedHashSet<Long>());
+		OperationalNode node1 = new OperationalNode(1, new Operator(OperatorRole.SRC, "gridsensor", x -> new Long(1000)), new ComputationalDemand(1, 1.0), new LinkedHashSet<Long>());
+		OperationalNode node2 = new OperationalNode(2, new Operator(OperatorRole.PIP, "selection1", x -> x/2), new ComputationalDemand(1, 1.0), new LinkedHashSet<Long>());
+		OperationalNode node3 = new OperationalNode(3, new Operator(OperatorRole.PIP, "selection2", x -> x/2), new ComputationalDemand(1, 1.0), new LinkedHashSet<Long>());
+		OperationalNode node4 = new OperationalNode(4, new Operator(OperatorRole.SNK, "datacenter", x -> new Long(1)), new ComputationalDemand(1, 1.0), new LinkedHashSet<Long>());
 		
-		app.addOperational(node1);
-		app.addOperational(node2);
-		app.addOperational(node3);
-		app.addOperational(node4);	
+		app.addStream(node1, node2);
+		app.addStream(node1, node3);
+		app.addStream(node2, node4);
+		app.addStream(node3, node4);
 		
-		app.addStream(1, 2);
-		app.addStream(1, 3);
-		app.addStream(2, 4);
-		app.addStream(3, 4);
-		app.addStream(2, 1);
+		app.addStream(node2, node1);
 		
 		return app;
 	}
