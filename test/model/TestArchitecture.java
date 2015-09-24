7b58b88fc6fa6f23cde6ee807a01a883646ce628
Giacomo Marciani <giacomo.marciani@gmail.com>
2015-09-24 10:44:46 +0200

    Test on architecture creation

--- test/model/TestArchitecture.java
+++ test/model/TestArchitecture.java
@@ -9,7 +9,8 @@ import model.architecture.node.ComputationalOffer;
 
 public class TestArchitecture {
 
-	@Test public void normal() {
+	@Test 
+	public void normal() {
 		Architecture arc = new Architecture("Sample Grid Architecture");
 		
 		ComputationalNode node1 = new ComputationalNode(1, "sensor1", new ComputationalOffer(1, 1, 1.0));
@@ -17,17 +18,17 @@ public class TestArchitecture {
 		ComputationalNode node3 = new ComputationalNode(3, "station3", new ComputationalOffer(1, 1, 1.0));
 		ComputationalNode node4 = new ComputationalNode(4, "datacenter", new ComputationalOffer(1, 1, 1.0));
 
-		arc.addComputationalNode(node1);
-		arc.addComputationalNode(node2);
-		arc.addComputationalNode(node3);
-		arc.addComputationalNode(node4);				
+		arc.addVertex(node1);
+		arc.addVertex(node2);
+		arc.addVertex(node3);
+		arc.addVertex(node4);		
 		
-		arc.addLogicalLink(1, 2, new LogicalLink(1.0, 1.0, 1.0));
-		arc.addLogicalLink(1, 3, new LogicalLink(1.0, 1.0, 1.0));
-		arc.addLogicalLink(2, 4, new LogicalLink(1.0, 1.0, 1.0));
-		arc.addLogicalLink(3, 4, new LogicalLink(1.0, 1.0, 1.0));
+		arc.addEdge(new LogicalLink(1.0, 1.0, 1.0), node1, node2);
+		arc.addEdge(new LogicalLink(1.0, 1.0, 1.0), node1, node3);
+		arc.addEdge(new LogicalLink(1.0, 1.0, 1.0), node2, node4);
+		arc.addEdge(new LogicalLink(1.0, 1.0, 1.0), node3, node4);
 		
 		System.out.println(arc);
 	}
-
+	
 }
