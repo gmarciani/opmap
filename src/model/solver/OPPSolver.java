9cd637af57a3c7aa97f740ab9bc648050e3beec1
Giacomo Marciani <giacomo.marciani@gmail.com>
2015-09-27 21:13:17 +0200

    New optimization solver structure

--- src/model/solver/OPPSolver.java
+++ src/model/solver/OPPSolver.java
@@ -1,11 +1,32 @@
 package model.solver;
 
 import control.exceptions.SolverException;
-import model.optmodel.OPPCPlexModel;
+import ilog.concert.IloException;
+import ilog.cplex.IloCplex;
+import model.optmodel.OPPModel;
 import model.report.Report;
 
 public interface OPPSolver {
 	
-	public Report solve(OPPCPlexModel model) throws SolverException;
+	public IloCplex getCPlex();
+	
+	public OPPModel getModel();
+	
+	default public Report solve(final OPPModel model) throws SolverException {
+		Report report = null;
+		double start, end;
+		
+		try {
+			start = model.getCPlexModel().getCplexTime();
+			if (model.getCPlexModel().solve()) {
+				end = model.getCPlexModel().getCplexTime();
+				report = new Report(model, start, end);
+			}				
+		} catch (IloException exc) {
+			throw new SolverException("Error while solving model: " + exc.getMessage());
+		}
+		
+		return report;
+	}
 
 }
--- src/model/solver/mp/MPSolver.java
+++ src/model/solver/mp/MPSolver.java
@@ -2,63 +2,31 @@ package model.solver.mp;
 
 import control.exceptions.SolverException;
 import ilog.concert.IloException;
-import ilog.concert.IloNumVar;
 import ilog.cplex.IloCplex;
-import model.optmodel.OPPCPlexModel;
-import model.report.Report;
+import model.optmodel.OPPModel;
 import model.solver.OPPSolver;
 
 public class MPSolver implements OPPSolver {
 	
-	private MPSolverParams params;
-	
-	public MPSolver(MPSolverParams params) {
-		this.setParams(params);
-	}
+	private IloCplex cplex;
+	private OPPModel model;
 
-	public MPSolver() {
-		this.setParams(MPSolverParams.getDefault());
+	public MPSolver() throws SolverException {
+		try {
+			this.cplex = new IloCplex();
+		} catch (IloException exc) {
+			throw new SolverException("Error while initiating CPlex: " + exc.getMessage());
+		}
 	}
 	
-	public MPSolverParams getParams() {
-		return this.params;
-	}
-
-	public void setParams(MPSolverParams params) {
-		this.params = params;
+	@Override
+	public IloCplex getCPlex() {
+		return this.cplex;
 	}
 
 	@Override
-	public Report solve(OPPCPlexModel model) throws SolverException {
-		Report report = new Report();
-		IloCplex cplex;
-		
-		try {
-			cplex = new IloCplex();
-		} catch (IloException exc) {
-			throw new SolverException("Error while initiating CPlex: " + exc.getMessage());
-		}		
-		
-		
-		try {
-			cplex.setModel(model.getCPlexModel());
-		} catch (IloException exc) {
-			throw new SolverException("Error while copying model: " + exc.getMessage());
-		}
-		
-		try {
-			if (cplex.solve()) {
-				report.setStatus(cplex.getStatus());
-				report.setObjectiveValue(cplex.getObjValue());
-				report.setSolutions(cplex.getValues((IloNumVar[])model.getVariables().toArray()));
-			}
-		} catch (IloException exc) {
-			throw new SolverException("Error while solving model: " + exc.getMessage());
-		}
-		
-		return report;
+	public OPPModel getModel() {
+		return this.model;
 	}
-
-	
 
 }
