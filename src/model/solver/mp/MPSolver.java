a489c0798a236a15afe3d4e01ffaa07221a3d534
Giacomo Marciani <giacomo.marciani@gmail.com>
2015-09-28 17:05:06 +0200

    New solver hierarchy

--- src/model/solver/mp/MPSolver.java
+++ src/model/solver/mp/MPSolver.java
@@ -2,17 +2,32 @@ package model.solver.mp;
 
 import control.exceptions.SolverException;
 import ilog.concert.IloException;
-import ilog.cplex.IloCplex;
+import model.optmodel.OPPModel;
+import model.report.Report;
 import model.solver.AbstractOPPSolver;
 
 public class MPSolver extends AbstractOPPSolver {
 
 	public MPSolver() throws SolverException {
+		super();
+	}
+	
+	@Override
+	public Report solve(OPPModel model) throws SolverException {
+		Report report = null;
+		double start, end;
+		
 		try {
-			super.setCPlex(new IloCplex());
+			start = model.getCPlexModel().getCplexTime();
+			if (model.getCPlexModel().solve()) {
+				end = model.getCPlexModel().getCplexTime();
+				report = new Report(model, start, end);
+			}				
 		} catch (IloException exc) {
-			throw new SolverException("Error while creating MPSolver " + exc.getMessage());
+			throw new SolverException("Error while solving model: " + exc.getMessage());
 		}
+		
+		return report;
 	}
 
 }
