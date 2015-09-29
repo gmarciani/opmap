10ce52f5d9f77a7185b575bed6be7b154b778050
Giacomo Marciani <giacomo.marciani@gmail.com>
2015-09-29 20:33:28 +0200

    Line Plot: creation, visualization and SVG exportation

--- test/plot/TestLinePlot.java
+++ test/plot/TestLinePlot.java
@@ -1,50 +1,41 @@
 package plot;
 
-
-import static org.junit.Assert.*;
-
 import java.io.IOException;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
+import org.jfree.chart.JFreeChart;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 import org.junit.Test;
 
-import view.LinePlot;
+import control.plotter.Plotter;
 
 public class TestLinePlot {
 
-	@Test public void createAndDisplay() throws InterruptedException {		
+	@Test 
+	public void createAndDisplay() throws InterruptedException {		
+		JFreeChart plot = Plotter.create("Sample Line Plot", "Domain", "Range", sampleDataset());
 		
-		LinePlot plot = new LinePlot("Sample Line Plot", "This is the sample plot subtitle", createDataset());
+		Plotter.display(plot);
 		
-		plot.display();
-		
-		TimeUnit.SECONDS.sleep(5);
-		
+		TimeUnit.SECONDS.sleep(5);		
 	}
 	
-	@Test public void createAndExport() {	
+	@Test 
+	public void createAndExport() throws IOException {		
+		JFreeChart plot = Plotter.create("Sample Line Plot", "Domain", "Range", sampleDataset());
 		
-		LinePlot plot = new LinePlot("Sample Line Plot", "This is the sample plot subtitle", createDataset());
-		
-		try {
-			plot.save("./test/plot/svg/svg-sample.svg", 500, 300);
-		} catch (IOException exc) {
-			fail("Plot SVG export failure: " + exc.getMessage());
-		}	
-		
+		Plotter.save(plot, "./test/plot/svg/svg-sample.svg");		
 	}
 	
-	private XYDataset createDataset() {
-		
+	private XYDataset sampleDataset() {		
 		Random rnd = new Random();		
         
-        final XYSeries series1 = new XYSeries("First");
-        final XYSeries series2 = new XYSeries("Second");
-        final XYSeries series3 = new XYSeries("Third");
+        XYSeries series1 = new XYSeries("First");
+        XYSeries series2 = new XYSeries("Second");
+        XYSeries series3 = new XYSeries("Third");
         
         for (int input = 100; input <= 1000; input+=100) {
         	double value = getRandom(rnd, 100, 1000000);
@@ -53,13 +44,12 @@ public class TestLinePlot {
         	series3.add(input, value * 100);
         }
 
-        final XYSeriesCollection dataset = new XYSeriesCollection();
+        XYSeriesCollection dataset = new XYSeriesCollection();
         dataset.addSeries(series1);
         dataset.addSeries(series2);
         dataset.addSeries(series3);
                 
         return dataset;        
-        
     }
 	
 	private double getRandom(Random rnd, double min, double max) {
